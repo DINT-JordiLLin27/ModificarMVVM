efbd99a3ccba51509d074adc6baab4a4ccd30119
Jorge <jorge.llinares@iesdoctorbalmis.com>
2020-01-17 10:03:02 +0100

    Agregar archivos de proyecto.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.DataContext = new MainWindowVM();
+            InitializeComponent();
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            string nombreCliente = (this.DataContext as MainWindowVM).GetClienteNombreCompleto();
+
+            MessageBoxResult boxResult = MessageBox.Show("¿Actualizar " + nombreCliente + "?", "Actualizar Dato", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
+
+            if (boxResult == MessageBoxResult.OK)
+            {
+                (this.DataContext as MainWindowVM).Modificar();
+                MessageBox.Show("Actualizado correctamente", "Actualizar Dato", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else MessageBox.Show("No se ha actualizado " + nombreCliente, "Actualizar Dato", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ModificarDatosComboBox.SelectedItem != null;
+        }
+    }
+}
---
+++ WpfApp4/MainWindowVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+
+        private InformesEntities contexto;
+        public CLIENTE CLIENTE_SELECCIONADO { get; set; }
+        public ObservableCollection<CLIENTE> LISTA_CLIENTES { get; set; }
+        public MainWindowVM()
+        {
+            contexto = new InformesEntities();
+            contexto.CLIENTES.Load();
+            LISTA_CLIENTES = contexto.CLIENTES.Local;
+        }
+
+        public void Modificar()
+        {
+            contexto.SaveChanges();
+        }
+
+        public string GetClienteNombreCompleto()
+        {
+            return CLIENTE_SELECCIONADO.nombre + " " + CLIENTE_SELECCIONADO.apellido;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
